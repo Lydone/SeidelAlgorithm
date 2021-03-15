32f1d8e11b7feabf11a3a25d252821b19e18e570
Valerii Belaventsev <vebelaventsev@gmail.com>
2021-03-15 12:24:42 +0300

    Add test

--- src/test/kotlin/SeidelAlgorithmTest.kt
+++ src/test/kotlin/SeidelAlgorithmTest.kt
@@ -112,16 +112,16 @@ internal class SeidelAlgorithmTest {
         )
         assertArrayEquals(
             arrayOf(
-                intArrayOf(0, 1, 0, 0),
-                intArrayOf(1, 0, 0, 0),
-                intArrayOf(0, 0, 0, 1),
-                intArrayOf(0, 0, 1, 0),
+                intArrayOf(0, 1, 1, 2),
+                intArrayOf(1, 0, 2, 3),
+                intArrayOf(1, 2, 0, 1),
+                intArrayOf(2, 3, 1, 0),
             ),
             SeidelAlgorithm.calculateDistanceMatrix(
                 arrayOf(
-                    intArrayOf(0, 1, 0, 0),
+                    intArrayOf(0, 1, 1, 0),
                     intArrayOf(1, 0, 0, 0),
-                    intArrayOf(0, 0, 0, 1),
+                    intArrayOf(1, 0, 0, 1),
                     intArrayOf(0, 0, 1, 0),
                 ),
             )
